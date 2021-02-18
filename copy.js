9e2e70425f95870f50d848edc29cd536d1f0c991
eduardo239 <eucrieiumaconta@gmail.com>
2021-02-18 15:10:25 -0300

    function copy adicionada

---
+++ copy.js
@@ -0,0 +1,21 @@
+const inpPassword = document.getElementById('result');
+const btnCopy = document.getElementById('copy');
+const pgMessageAlert = document.getElementById('pg-message-alert');
+
+const copyToClipboard = () => {
+  const el = document.createElement('textarea');
+  el.value = inpPassword.value;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  pgMessageAlert.classList.remove('is-hidden');
+  setTimeout((i) => {
+    pgMessageAlert.classList.add('is-hidden');
+  }, 2500);
+};
+
+btnCopy.addEventListener('click', copyToClipboard);
